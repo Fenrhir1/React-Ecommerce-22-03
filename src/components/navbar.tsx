67bb008741de4e4aa2b451e305ad72586e60c072
Salvatore Polizzotti <fenrhir@libero.it>
2024-03-25 12:10:33 +0100

    merged cart logic

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,6 @@ import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import Cart from "./Cart";
 const pages = ["Products"];
